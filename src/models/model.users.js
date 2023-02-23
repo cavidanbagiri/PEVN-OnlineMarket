d160fc07a586760e46c9fdf627347dc14dd5e356
cavidanbagiri <cavidanbagiri@gmail.com>
2023-02-23 20:55:59 +0300

    Creating User Model

---
+++ src/models/model.users.js
@@ -0,0 +1,30 @@
+
+
+module.exports = (sequelize, DataTypes, Model) => {
+
+    class UserModel extends Model{}
+
+    UserModel.init({
+        username:{
+            type: DataTypes.STRING,
+            allowNull: true,
+            defaultValue: "Unknown"
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        modelName: 'users'
+    })
+
+    return UserModel;
+
+}
+
