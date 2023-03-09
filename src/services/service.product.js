ccb2fdd832b12ab303143c246679e1876bfd14d6
cavidanbagiri <cavidanbagiri@gmail.com>
2023-03-09 06:16:40 +0300

    Change Get Product

--- src/services/service.product.js
+++ src/services/service.product.js
@@ -1,47 +1,68 @@
-
-const db = require('../models/index');
+const db = require("../models/index");
 
 const ProductModel = db.ProductModel;
 const BasketModel = db.BasketModel;
 const FavoritesModel = db.FavoriteModel;
 
 class ProductService {
+  static async getProductWithId(product_id) {
+    const temp_pro = await db.sequelize.query(
+      "select *, p.id from products p " +
+        'left JOIN comments c ON "productId"=$1 ' +
+        'left JOIN users u ON "userId"=u.id ' +
+        "where p.id = $1",
+      {
+        bind: [product_id],
+      }
+    );
 
-    static async getProductWithId(product_id){
+    console.log("temp pro : ", temp_pro[0][0]);
+    return temp_pro[0][0];
 
-        // const temp_pro = await db.sequelize.query('select * from products LEFT JOIN comments ON \"productId\"=$1 where products.id = $1',{
-        //     bind:[product_id]
-        // });
+    // const product = await ProductModel.findByPk(product_id);
+    // return product.dataValues;
+  }
 
-        // console.log('temp pro : ',temp_pro[0][0]);
-        // return temp_pro[0][0];
+  static async addBasket(product_id) {
+    const adding_product = await BasketModel.create({
+      userId: 14,
+      productId: product_id,
+    });
 
-        const product = await ProductModel.findByPk(product_id);
-        return product.dataValues;
+    return adding_product;
+  }
 
-    }
+  static async addFavorites(product_id) {
+    const adding_product = await FavoritesModel.create({
+      userId: 14,
+      productId: product_id,
+    });
 
-    static async addBasket(product_id){
-
-        const adding_product = await BasketModel.create({
-            userId:14,
-            productId:product_id
-        });
-
-        return adding_product;
-    }
-
-    static async addFavorites(product_id){
-
-        const adding_product = await FavoritesModel.create({
-            userId:14,
-            productId:product_id
-        });
-
-        return adding_product;
-    }
-
+    return adding_product;
+  }
 }
 
-
 module.exports = ProductService;
+
+// const temp_pro = await db.sequelize.query(
+// 'select *, p.id from products p ' +
+// 'left JOIN comments c ON \"productId\"=$1 ' +
+// 'where p.id = $1',
+// {
+//     bind:[product_id]
+// });
+
+// const temp_pro = await db.sequelize.query(
+//     'select *, p.id from products p ' +
+//     'left JOIN comments c ON \"productId\"=$1 ' +
+//     'left JOIN users u ON \"c.userId\"=u.id ' +
+//     'where p.id = $1',
+//     {
+//         bind:[product_id]
+//     });
+
+// const temp_pro = await db.sequelize.query(
+//     'select *, p.id from products p ' +
+//     'left JOIN comments c ON \"productId\"=2' +
+//     'left JOIN users u ON \"userId\"=u.id ' +
+//     'where p.id = 2');
